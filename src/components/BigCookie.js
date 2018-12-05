9420ab60fa387995481544925890683faf018483
Jake Ravens <jakearavens@gmail.com>
2018-12-04 22:11:24 -0500

    changed BigCookie into a functional component

--- src/components/BigCookie.js
+++ src/components/BigCookie.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 function BigCookie(props) {
 
-    render() {
-        return(
-            <div 
-                className='bigcookie' 
-                onClick={props.cookieClick}
-            >
-                <h2>Click Me</h2>
-            </div>
-        );
-    }
+    return(
+        <div 
+            className='bigcookie' 
+            onClick={props.cookieClick}
+        >
+            <h2>Click Me</h2>
+        </div>
+    )
 }
 
 export default BigCookie;
